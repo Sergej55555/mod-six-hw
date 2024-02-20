efa7bc1af691f8b792e6d3045b757b80c22b60ff
Sergej55555 <smisenkov137@gmail.com>
2024-02-20 23:15:35 +1000

    my module six hw

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [bike, setBikes] = useState(data);
+  const [showBike, setShowBike] = useState(false);
+
+  const removeBike = (id) => {
+  let newBikes = bike.filter((bike) => bike.id !== id)
+  setBikes(newBikes)
+  }
+
+  const showBikeClick = (item) => {
+    item.showMore = !item.showMore
+    setShowBike(!showBike)
+  }
+ 
+  return (<div>
+    <div className='container'>
+      <h1>Most beutiful {bike.length} bikes</h1>
     </div>
-  );
+
+    {bike.map((item => {
+      const {id, name, description, image, showMore } = item 
+
+      return (<div key={id}> 
+          <div className='container'>
+          <p>{id} - {name}</p> 
+          </div>
+          <div className='container'>
+          <p>{showMore ? description : description.substring(0, 230) + '...'} 
+          <button onClick={() => showBikeClick(item)}>{showMore ? 'show less' : 'show more'}</button>
+          </p>
+          </div>
+          <div className='container'>
+          <img src={image} width='350px' alt='Bikes'/>
+          </div>
+
+          <br></br>
+
+          <div className='container'>
+           <button className='btn' onClick={() => removeBike(id)}>Remove bike</button>
+          </div>
+
+        </div>
+      )
+    }))}
+
+    <br></br>
+    <br></br>
+
+    <div className='container'>
+    <button className='btn' onClick={() => setBikes([])}>Delete All</button>
+    </div>
+  </div>
+  )
 }
 
 export default App;
